be56ce99e3609fb7c3c60f296d59836ffdf2351b
Aaron Ashmore <aaashmor@iu.edu>
2024-10-08 11:27:37 -0400

    Week7Console

---
+++ Week7/console.js
@@ -0,0 +1,31 @@
+console.log("hi");
+
+let myAge = 52;
+
+myAge = 53;
+
+console.log(myAge);
+
+let myDog = {
+    hairColor: 'green',
+    length: '10ft 3in',
+    teethcount: 174,
+    attribute: ["cuddly", "soft"],
+    name: "Franklin High School",
+};
+
+console.log(myDog);
+
+console.log("Dog Name", myDog.name);
+console.log("Dog Friends:", myDog.friends);
+
+// Your Name, Your Favorite Number, Came to class today?, a monster and its 3 features, 
+
+const myName = "Aaron";
+const favNum = 44;
+let present = true;
+let sully = {
+    sullyFurColor: 'Blue',
+    sullyHeight: 7,
+    sullyScares: "100%",
+};
