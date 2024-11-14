ac6f3236739c44631dc14219d2201e47242e6f4a
Aaron Ashmore <aaashmor@iu.edu>
2024-11-14 11:00:17 -0500

    Week12

---
+++ Week12/connectFirebase.js
@@ -0,0 +1,44 @@
+import { app } from "./firebase.js";
+
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+
+const booksListRef = document.querySelector("#booksList");
+
+const db = getFirestore(app);
+
+async function getBooks() {
+  const booksCollection = collection(db, "books");
+  const booksDocs = await getDocs(booksCollection);
+
+  booksListRef.innerHTML = "";
+  for (let i = 0; i < booksDocs.docs.length; i++) {
+    const book = booksDocs.docs[i];
+    const bookData = book.data();
+
+    console.log(book.id, book.data());
+
+    booksListRef.innerHTML += `<li>${bookData.title} (${bookData.yearPublished})</li>`;
+  }
+}
+
+async function addBook() {
+  const bookName = prompt("Book Name:");
+  const bookYear = prompt("Year Name:");
+
+  const booksCollection = collection(db, "books");
+
+  await addDoc(booksCollection, { title: bookName, yearPublished: bookYear });
+  
+
+  getBooks();
+  
+}
+
+document.querySelector("#addBookButton").onclick = addBook;
+
+getBooks();
